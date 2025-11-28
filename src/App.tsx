4e1ce5c7bb689405d90abc66b505a4f650aaa1dc
dailyatti <dailyatti@gmail.com>
2025-11-28 23:05:37 +0100

    fix: restore responsive image grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1216,7 +1216,7 @@ const App: React.FC = () => {
                     )}
                 </AnimatePresence>
 
-                <div className="grid grid-cols-1 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     <AnimatePresence>
                         {images.map(img => (
                             <ImageCard
